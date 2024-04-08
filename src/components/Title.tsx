e2fcee4cfff4f08c15bbfb3205606171c4abdd58
Inter <hi@iinter.me>
2024-04-08 19:40:30 +0100

    🧹 chore: make full placeholder text show on mobile

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -9,7 +9,7 @@ export default function Title({ title, setTitle }) {
     <textarea
       value={title}
       onChange={handleChange}
-      className="bg-transparent focus:bg-neutral-800 focus:bg-opacity-50 duration-500 rounded-lg text-zinc-400 placeholder-neutral-700 text-2xl outline-none w-full p-2 h-11 overflow-hidden font-bold resize-none"
+      className="bg-transparent focus:bg-neutral-800 focus:bg-opacity-50 duration-500 rounded-lg text-zinc-400 placeholder-neutral-700 md:text-2xl text-lg outline-none w-full p-2 h-11 overflow-hidden font-bold resize-none"
       style={{ fontFamily: "'Geist Mono', monospace", border: 'none' }}
       placeholder="Give this note a title..."
     />
