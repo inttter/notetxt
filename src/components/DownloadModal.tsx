ea05720f85b9e37f2d19ee4146d3a8d9a80181c7
Inter <hi@iinter.me>
2024-07-10 22:56:53 +0100

    💄 chore(style): redesign download modal to match command menu

--- src/components/DownloadModal.tsx
+++ src/components/DownloadModal.tsx
@@ -16,32 +16,32 @@ function Modal({ isVisible, onClose, onSave }) {
     <AnimatePresence>
       {isVisible && (
         <motion.div
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
+          initial={{ opacity: 0, scale: 0.9 }}
+          animate={{ opacity: 1, scale: 1.0 }}
           exit={{ opacity: 0 }}
-          transition={{ duration: 0.2 }}
-          className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50 p-4"
+          transition={{ type: 'spring', stiffness: 500, damping: 30 }}
+          className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50 px-6 md:px-0"
         >
-          <div className="bg-neutral-900 p-6 rounded-md shadow-2xl shadow-neutral-950 max-w-xl w-full">
-            <div className="text-xl text-zinc-100 mb-4 ml-1">
-              Enter a title for your note
+          <div className="bg-neutral-900 p-5 rounded-lg shadow-2xl shadow-neutral-950 max-w-xl w-full">
+            <div className="text-xl text-zinc-300 mx-0.5">
+              Enter a title for your note:
             </div>
             <input
               type="text"
               value={fileName}
               onChange={(e) => setFileName(e.target.value)}
-              className="w-full p-2 border-2 border-neutral-800 focus:border-neutral-600 duration-300 text-zinc-300 placeholder:text-neutral-600 rounded-md outline-none mb-4"
-              placeholder="My New Note"
+              className="w-full px-3 py-2 rounded-md bg-neutral-900 focus:bg-neutral-800 focus:bg-opacity-60 placeholder:text-neutral-500 text-zinc-100 outline-none tracking-tight border border-neutral-700 focus:border-neutral-600 duration-300 mt-3 mb-3.5"
+              placeholder="Note Title"
             />
             <div className="flex justify-between items-center">
-              <div className="text-xs text-neutral-600 italic hidden md:block mb-2 md:mb-0">
-                If you don't want to give it a name, leave the text field empty.
+              <div className="text-xs text-neutral-600 italic hidden md:block ml-0.5 mb-2 md:mb-0">
+                Leave empty to give the note no name.
               </div>
-              <div>
-                <button onClick={onClose} className="bg-neutral-700 hover:bg-neutral-600 duration-300 text-zinc-100 px-4 py-1.5 rounded-md mr-2">
+              <div className="text-zinc-100">
+                <button onClick={onClose} className="bg-[#282828] border border-transparent hover:border-neutral-700 hover:bg-neutral-600 hover:bg-opacity-40 hover:cursor-pointer duration-300 mr-2 px-4 py-1 rounded-md">
                   Cancel
                 </button>
-                <button onClick={handleSave} className="bg-green-700 hover:bg-green-600 duration-300 text-zinc-100 px-4 py-1.5 rounded-md mr-0.5">
+                <button onClick={handleSave} className="bg-[#282828] border border-transparent hover:border-neutral-700 hover:bg-neutral-600 hover:bg-opacity-40 hover:cursor-pointer duration-300 px-4 py-1 rounded-md mr-0.5">
                   Save
                 </button>
               </div>
