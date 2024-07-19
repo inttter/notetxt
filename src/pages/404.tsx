9250edb304a150590607892384b9cc6580f4cfcd
Inter <hi@iinter.me>
2024-07-19 21:17:11 +0100

    🧹 chore(404): slightly tweak 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { CircleX, ArrowLeft } from 'lucide-react';
+import { X, ArrowLeft } from 'lucide-react';
 import '@fontsource/geist-mono';
 import '@fontsource/geist-sans';
 import Link from 'next/link';
@@ -9,18 +9,20 @@ export default function Custom404() {
   return (
     <>
       <Head>
-        <title>404 • notetxt</title>
+        <title>404 • Notetxt</title>
       </Head>
       <div className="bg-[#101010] min-h-screen flex flex-col justify-center items-center antialiased scroll-smooth p-4 md:p-8">
         <div className="max-w-2xl w-full px-4 py-8 space-y-4 flex-col">
           <div className="flex items-center justify-start">
-            <CircleX size={40} className="mr-2 text-red-400 md:mt-0 mt-10" />
-            <h1 className="text-4xl md:text-5xl text-zinc-100 opacity-90 md:mt-0 mt-10 max-w-md font-semibold tracking-tighter border-b-2 border-dotted border-neutral-700 flex items-center">
+            <X size={40} className="mr-2 text-red-400 md:mt-0 mt-10" />
+            <h1 className="text-4xl md:text-5xl text-zinc-100 opacity-90 md:mt-0 mt-10 max-w-md font-semibold tracking-tighter flex items-center">
               Page Not Found
             </h1>
           </div>
           <p className="text-neutral-500 text-lg max-w-lg py-2">
-            Sorry, the page you tried to navigate to couldn't be found. The link may have been moved or deleted.
+            Sorry, this page does not exist. 
+            It may have been changed, moved or deleted.
+            Click 'Back' to go back to the main page.
           </p>
           <button className="text-zinc-300 bg-[#202020] hover:bg-neutral-600 hover:bg-opacity-40 duration-300 px-4 py-2 rounded-md group">
             <Link href="/" className="flex items-center">
