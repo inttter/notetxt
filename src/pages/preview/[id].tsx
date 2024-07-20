0d42e2a3c36e9e741e92117f661cb788af67b489
Inter <hi@iinter.me>
2024-07-20 13:52:23 +0100

    💄 style(preview): add animations

--- src/pages/preview/[id].tsx
+++ src/pages/preview/[id].tsx
@@ -5,6 +5,7 @@ import remarkGfm from 'remark-gfm';
 import rehypeRaw from 'rehype-raw';
 import { ArrowLeft } from 'lucide-react';
 import Link from 'next/link';
+import { motion } from 'framer-motion';
 
 // markdown styling
 // note: uses github markdown styling + my own custom styling from markdownStyles
@@ -49,17 +50,26 @@ const Preview = () => {
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-[#0D1117] p-4 sm:p-6 lg:p-8 overflow-x-hidden">
-      <div className="w-full max-w-2xl">
+
+      <motion.div 
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.5, delay: 0.1 }}
+        className="w-full max-w-2xl"
+      >
         <div className="flex justify-start mb-4 md:pt-0 pt-5">
           <button className="text-sm text-zinc-300 hover:text-zinc-400 duration-300 rounded-md group">
             <Link href="/" className="flex items-center">
-              <ArrowLeft size={20} className="mr-1 group-hover:-translate-x-0.5 text-zinc-300 group-hover:text-zinc-400 duration-300" /> Back to main page
+              <ArrowLeft size={20} className="mr-1 group-hover:-translate-x-0.5 text-zinc-100 group-hover:text-zinc-400 duration-300" /> Return to note
             </Link>
           </button>
         </div>
-      </div>
+      </motion.div>
 
-      <div
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.5, delay: 0.3 }}
         className="prose prose-invert text-zinc-300 w-full max-w-2xl mx-auto"
         style={{ fontFamily: 'Geist Sans, sans-serif' }}
       >
@@ -70,7 +80,7 @@ const Preview = () => {
           rehypePlugins={[rehypeRaw]}
           components={markdownStyles}
         />
-      </div>
+      </motion.div>
     </div>
   );
 };
