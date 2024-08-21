48dc0f04a9f8da5a5236fd83da8a420d4b6bd772
Inter <hi@iinter.me>
2024-08-21 13:27:40 +0100

    🧹 chore: seperate `Head` content from `_app.tsx`

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,8 @@
-import Head from 'next/head';
+import React from 'react';
 import { AppProps } from 'next/app';
 import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/next';
+import Head from 'next/head';
 import '../styles/index.css';
 import '../styles/github-alerts.css';
 import '@fontsource/geist-sans';
@@ -9,29 +10,19 @@ import '@fontsource/jetbrains-mono';
 import 'github-markdown-css';
 import { TextProvider } from '../components/markdown/TextContent';
 
+
+// Prevents 'useEffectLayout does nothing on the server' warning that comes from Vaul
+// Source: https://github.com/emilkowalski/vaul/issues/367 and https://github.com/emilkowalski/vaul/pull/368
+// https://stackoverflow.com/questions/58070996/how-to-fix-the-warning-uselayouteffect-does-nothing-on-the-server
+React.useLayoutEffect = React.useEffect 
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-    <Head>
-      <title>Notetxt</title>
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      <meta property="og:title" content="Notetxt" />
-      <meta property="og:type" content="website" />
-      <meta property="og:image" content="https://notetxt.xyz/external/og.png" />
-      <meta property="og:description" content="A note-taker app with a focus on minimalism and simplicity, so you can write down what's on your mind." />
-      <meta name="description" content="A note-taker app with a focus on minimalism and simplicity, so you can write down what's on your mind. Now with Markdown support!" />
-      <meta name="twitter:image" content="https://notetxt.xyz/external/og.png" />
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="theme-color" content="#161617" />
-      <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
-      <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
-      <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
-      <link rel="icon" type="image/png" sizes="192x192" href="/favicon/android-chrome-192x192.png" />
-      <link rel="icon" type="image/png" sizes="512x512" href="/favicon/android-chrome-512x512.png" />
-      <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#5bbad5" />
-      <link rel="shortcut icon" href="/favicon/favicon.ico" />
-      <link href="https://cdn.jsdelivr.net/npm/katex@0.16.11/dist/katex.min.css" rel="stylesheet" />
-    </Head>
+      <Head>
+        <title>Notetxt</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
       <TextProvider>
         <div className="selection:bg-neutral-700 selection:text-zinc-300">
           <Component {...pageProps} />
---
+++ src/pages/_document.js
@@ -0,0 +1,30 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <meta property="og:title" content="Notetxt" />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content="https://notetxt.xyz/external/og.png" />
+        <meta property="og:description" content="A note-taker app with a focus on minimalism and simplicity, so you can write down what's on your mind." />
+        <meta name="description" content="A note-taker app with a focus on minimalism and simplicity, so you can write down what's on your mind. Now with Markdown support!" />
+        <meta name="twitter:image" content="https://notetxt.xyz/external/og.png" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="theme-color" content="#161617" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+        <link rel="icon" type="image/png" sizes="192x192" href="/favicon/android-chrome-192x192.png" />
+        <link rel="icon" type="image/png" sizes="512x512" href="/favicon/android-chrome-512x512.png" />
+        <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#5bbad5" />
+        <link rel="shortcut icon" href="/favicon/favicon.ico" />
+        <link href="https://cdn.jsdelivr.net/npm/katex@0.16.11/dist/katex.min.css" rel="stylesheet" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
