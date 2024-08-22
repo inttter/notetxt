b26fcf10cd79082845fffda551152acf20eeab0c
Inter <hi@iinter.me>
2024-08-22 13:39:50 +0100

    🧹 chore: add github link to footer

--- src/components/Landing/Footer.tsx
+++ src/components/Landing/Footer.tsx
@@ -12,13 +12,17 @@ const Footer = () => (
     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
       <div className="flex flex-col items-center text-center">
         <p className="text-sm text-center">
-          © 2024 Inter —{' '}
-          <Link href="https://github.com/inttter/notetxt/blob/master/LICENSE" className="text-stone-400 hover:text-stone-300 duration-300">
+          © 2024 / {' '}
+          <Link href="https://github.com/inttter/notetxt/blob/master/LICENSE" target="_blank" rel="noopener noreferrer" className="text-stone-400 hover:text-stone-300 duration-300">
             MIT License
           </Link>{' '}
-          —{' '}
+          /{' '}
           <Link href="/privacy" className="text-stone-400 hover:text-stone-300 duration-300">
             Privacy
+          </Link>{' '}
+          /{' '}
+          <Link href="https://github.com/inttter/notetxt" target="_blank" rel="noopener noreferrer" className="text-stone-400 hover:text-stone-300 duration-300">
+            GitHub
           </Link>
         </p>
       </div>
