e43cbe0362273684731bf5ffc2e473d75bf1921a
Inter <hi@iinter.me>
2024-08-28 12:59:01 +0100

    🧹 chore(landing): update badge

--- src/components/Landing/Badge.tsx
+++ src/components/Landing/Badge.tsx
@@ -4,12 +4,12 @@ import { motion } from 'framer-motion';
 const Badge = () => (
   <motion.div
     className="mb-8 flex justify-center"
-    initial={{ opacity: 0, y: -10 }} 
+    initial={{ opacity: 0, y: -20 }} 
     animate={{ opacity: 1, y: 0 }} 
-    transition={{ duration: 0.5, delay: 0.2 }}
+    transition={{ duration: 0.5, delay: 0.4 }}
   >
     <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-zinc-300 bg-default/60 ring-1 ring-neutral-700/70 shadow-lg shadow-neutral-950 duration-300 overflow-hidden whitespace-nowrap text-ellipsis" role="alert" aria-label="Badge">
-      📝 Markdown previews are now in the Editor!
+      📚 You can now export all of your notes!
     </div>
   </motion.div>
 );
