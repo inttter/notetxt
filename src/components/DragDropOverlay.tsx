a83178b205dac2525dffac244c586445fe174f3a
Inter <hi@iinter.me>
2024-09-01 14:07:39 +0100

    🧹 chore: update description of drag and drop overlay
    It doesn't override the current note anymore.

--- src/components/DragDropOverlay.tsx
+++ src/components/DragDropOverlay.tsx
@@ -4,6 +4,9 @@ interface DragDropOverlayProps {
   isDraggingOver: boolean;
 }
 
+const dragDropTitle = 'Drop to import the file\'s content'
+const dragDropDescription = 'This will automatically create a new note once you drop it in.'
+
 const DragDropOverlay: React.FC<DragDropOverlayProps> = ({ isDraggingOver }) => {
   return (
     <>
@@ -13,12 +16,12 @@ const DragDropOverlay: React.FC<DragDropOverlayProps> = ({ isDraggingOver }) => 
       <div
         className={`fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-40 transition-opacity duration-300 z-50 p-8 pointer-events-none ${isDraggingOver ? 'opacity-100' : 'opacity-0'}`}
       >
-        <div className="text-center bg-dark border border-neutral-800 px-4 py-4 rounded-md">
-          <div className="text-2xl text-zinc-300 tracking-tight">
-            Drop to import the file's content
+        <div className="text-center bg-dark border border-neutral-800 px-4 py-4 rounded-lg">
+          <div className="text-2xl text-zinc-300 tracking-tight" aria-label="Drag And Drop Overlay Title">
+            {dragDropTitle}
           </div>
-          <div className="mt-2 text-sm text-stone-400">
-            Please note that this will override all current content once you drop it in.
+          <div className="mt-1 text-sm text-stone-400" aria-label="Drag And Drop Overlay Description">
+            {dragDropDescription}
           </div>
         </div>
       </div>
