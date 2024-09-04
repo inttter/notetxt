1127ebc35439b0e076c2252f3418942974c81af1
Inter <hi@iinter.me>
2024-09-04 15:22:58 +0100

    💄 style(tip): add animations to tip

--- src/components/Tip.tsx
+++ src/components/Tip.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { motion } from 'framer-motion';
 import { Lightbulb } from 'lucide-react';
 
 export default function Home() {
@@ -27,11 +28,16 @@ export default function Home() {
 
   return (
     <div className="relative">
-      <div className="absolute bottom-7 md:bottom-5 left-1/2 md:left-2 transform -translate-x-1/2 md:translate-x-0 flex items-center justify-center md:justify-start w-full md:w-auto px-4">
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.5, delay: 0.1 }}
+        className="absolute bottom-7 md:bottom-5 left-1/2 md:left-2 transform -translate-x-1/2 md:translate-x-0 flex items-center justify-center md:justify-start w-full md:w-auto px-4"
+      >
         <span className="text-xs text-stone-400 text-center md:text-left flex items-center break-words overflow-hidden">
           <Lightbulb size={15} className="mr-1" /> {randomTip}
         </span>
-      </div>
+      </motion.div>
     </div>
   );
 }
