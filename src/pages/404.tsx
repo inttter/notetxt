bd50deb1d63968e933c82fbb213fa81d175375fe
Inter <hi@iinter.me>
2024-09-17 22:09:11 +0100

    💄 style: redesign 404 page
    - Now matches the other parts of the website quite a bit better in terms of styling
    - Swapped 'Page Not Found' for '404'
    - Updated description to be less bland/boring
    - Updated button to direct back to the Editor (`/editor`) when clicked
    - Added animations to the page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
-import { X, ArrowLeft } from 'lucide-react';
+import { ArrowLeft } from 'lucide-react';
+import { motion } from 'framer-motion';
+import Link from 'next/link';
 import Head from 'next/head';
+
+const pageTitle404 = '404';
+const pageDescription404 = `Unfortunately, you won't stumble upon your notes on this page. Check the link you entered, or use the button below to go back to the Editor, where your notes actually are.`;
 
 export default function Custom404() {
   return (
@@ -8,24 +13,45 @@ export default function Custom404() {
       <Head>
         <title>404 • Notetxt</title>
       </Head>
-      <div className="bg-[#101010] min-h-screen flex flex-col justify-center items-center antialiased scroll-smooth p-4 md:p-8">
+
+      <div className="bg-dark min-h-screen flex flex-col justify-center items-center antialiased scroll-smooth p-4 md:p-8">
         <div className="max-w-2xl w-full px-4 py-8 space-y-4 flex flex-col items-center">
-          <div className="flex items-center justify-center">
-            <X size={40} className="mr-2 text-red-400" />
-            <h1 className="text-4xl md:text-5xl text-zinc-100 opacity-90 max-w-md tracking-tighter">
-              Page Not Found
-            </h1>
-          </div>
-          <p className="text-stone-400 text-base max-w-md py-2 text-center">
-            Sorry, the page you were looking for does not exist or was moved elsewhere.
-            Click on the button below to go back to the previous page you were on.
-          </p>
-          <button className="btn text-sm text-zinc-300 bg-[#282828] bg-opacity-80 border-2 border-neutral-700 border-opacity-40 hover:bg-opacity-60 flex items-center group mt-4">
-            <div className="flex items-center" onClick={() => window.history.back()}>
-              <ArrowLeft size={20} className="mr-1 group-hover:-translate-x-0.5 duration-300" /> 
-              Go back
-            </div>
-          </button>
+          
+          <motion.div
+            initial={{ opacity: 0, y: -20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5, ease: 'easeOut' }}
+            className="flex items-center justify-center text-8xl font-semibold text-zinc-100 tracking-tighter md:tracking-normal"
+            aria-label="404 Page Title"
+          >
+            {pageTitle404}
+          </motion.div>
+
+          <motion.div
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5, ease: 'easeOut', delay: 0.2 }}
+            className="text-stone-400/90 text-base max-w-lg pt-1 pb-3 text-center"
+            aria-label="404 Page Description"
+          >
+            {pageDescription404}
+          </motion.div>
+          
+          <motion.div
+            initial={{ opacity: 0, y: 10 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5, ease: 'easeOut', delay: 0.2 }}
+          >
+            <Link 
+              href="/editor" 
+              className="btn text-zinc-300 bg-neutral-800/70 hover:bg-neutral-800/50 text-base border border-neutral-700/70 hover:border-neutral-600/80 active:border-neutral-500/70 duration-300 rounded-lg shadow-md shadow-neutral-950 flex items-center group"
+              passHref
+            >
+              <ArrowLeft size={17} className="mr-1 text-stone-400 group-hover:-translate-x-0.5 duration-300" /> 
+              Go back to the Editor
+            </Link>
+          </motion.div>
+
         </div>
       </div>
     </>
