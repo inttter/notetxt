25a8b0821cb7e7f49ff619f5370c7a5615168897
Inter <hi@iinter.me>
2024-10-01 22:43:06 +0100

    🧹 chore(landing): update landing page images and data
    Updated all the landing page images to not be out-of-date due to new changes.
    
    Also updated some of the JSON data with new and a bit of slightly reworded info.

--- src/components/Landing/Badge.tsx
+++ src/components/Landing/Badge.tsx
@@ -5,7 +5,7 @@ import { ArrowRight } from 'lucide-react';
 
 // ? To make the badge link somewhere, add a link in the speech marks after `link =`.
 // ? To not wrap the badge in a link, leave the text in between the speech marks blank.
-const Badge = ({ link = "https://github.com/inttter/notetxt?tab=readme-ov-file#supported-slash--commands" }) => {
+const Badge = ({ link = "" }) => {
   const content = (
     <div 
       className={`rounded-lg px-3 py-1 md:py-1.5 text-xs md:text-sm leading-6 text-zinc-300 bg-default/60 ring-1 
@@ -16,7 +16,7 @@ const Badge = ({ link = "https://github.com/inttter/notetxt?tab=readme-ov-file#s
       aria-label="Badge"
     >
       <div className="text-ellipsis overflow-hidden whitespace-nowrap">
-        ✨ NEW: Slash Commands are now in the Editor!
+        🏷️ You can now finally add tags to your notes!
       </div>
       {link && <ArrowRight size={15} className="ml-1 text-stone-400" />}
     </div>
