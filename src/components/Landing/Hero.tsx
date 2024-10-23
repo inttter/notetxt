e178242621a0a733749876ee778377a4830365a0
Inter <hi@iinter.me>
2024-10-23 22:21:01 +0100

    🧹 chore(landing): adjust hero section text
    Also reworded a couple of the feature cards.

--- src/components/Landing/Hero.tsx
+++ src/components/Landing/Hero.tsx
@@ -3,10 +3,10 @@ import Link from 'next/link';
 import { ArrowRight, BookOpenText, NotepadText } from 'lucide-react';
 import { motion } from 'framer-motion';
 
-const heroMessage = `Write down what's on your mind.`
-const heroDescription = `A minimalistic, simple note-taker app designed to help you jot down what you need with no distractions, useful features, and a minimal user interface, right at your fingertips.`
+const heroMessage = `Write down what's on your mind.`;
+const heroDescription = `A minimalistic note-taker app designed to let you write down what you need with no distractions, useful features, and a simple user interface, right at your fingertips.`;
 
-// Starts with opacity 0.01 to make Lighthouse work: https://stackoverflow.com/questions/55826735/how-to-fix-lighthouse-r eturned-error-no-fcp-when-running-google-page-speed-t
+// Starts with opacity 0.01 to make Lighthouse work: https://stackoverflow.com/questions/55826735/how-to-fix-lighthouse-returned-error-no-fcp-when-running-google-page-speed-t
 const HeroSection = () => (
   <motion.div
     initial={{ opacity: 0.01, y: -20 }} 
@@ -14,25 +14,27 @@ const HeroSection = () => (
     transition={{ duration: 0.5, delay: 0.4 }}
     className="text-center"
   >
-    <div className="text-4xl sm:text-7xl font-semibold tracking-tighter md:tracking-normal text-zinc-100">
+    <div className="text-4xl sm:text-7xl font-semibold tracking-tighter md:tracking-normal text-zinc-100" aria-label="Hero Message">
       {heroMessage}
     </div>
-    <div className="mt-6 text-md md:text-lg leading-6 text-stone-300">
+    <div className="mt-6 text-md md:text-lg leading-6 text-stone-300" aria-label="Hero Description">
       {heroDescription}
     </div>
     <div className="mt-8 flex items-center justify-center gap-x-5">
       <Link 
         href="/editor" 
         className="px-3 py-2 text-zinc-300 hover:text-zinc-100 bg-neutral-800/70 text-base border border-neutral-700/70 hover:bg-opacity-60 hover:border-neutral-600 active:border-neutral-500 duration-300 rounded-lg shadow-md shadow-neutral-950 flex items-center group"
+        aria-label="Open Editor Button"
       >
         <NotepadText size={17} className="mr-1 text-stone-400 group-hover:text-zinc-100 rotate-6 duration-300" /> 
-        Go to the Editor
+        Open the Editor
       </Link>
       <Link 
         href="https://docs.notetxt.xyz" 
         target="_blank" 
         rel="noopener noreferrer" 
         className="text-sm text-zinc-300 hover:text-zinc-100 duration-300 flex items-center group"
+        aria-label="Visit Documentation Button"
       >
         <BookOpenText size={17} className="mr-1 text-stone-400 group-hover:text-zinc-100 duration-300" /> 
         Visit the Docs
