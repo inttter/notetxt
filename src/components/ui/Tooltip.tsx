a51aa54ddb544029e883dd0ded1a2039c0d4f85f
Inter <hi@iinter.me>
2024-12-22 15:20:09 +0000

    🐛 fix: fix tooltips appearing above overlays

--- src/components/ui/Tooltip.tsx
+++ src/components/ui/Tooltip.tsx
@@ -20,7 +20,7 @@ const TooltipContent = React.forwardRef<
       ref={ref}
       sideOffset={sideOffset}
       className={cn(
-        "z-50 overflow-hidden rounded-md bg-neutral-900 border border-neutral-800 px-2 py-1.5 text-xs text-zinc-200 font-medium animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 selection:bg-neutral-700 selection:text-zinc-300",
+        "z-40 overflow-hidden rounded-md bg-neutral-900 border border-neutral-800 px-2 py-1.5 text-xs text-zinc-200 font-medium animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 selection:bg-neutral-700 selection:text-zinc-300",
         className
       )}
       {...props}
