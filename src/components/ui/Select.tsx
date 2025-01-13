d58b852b13f9cced393e2853676917842a9f65a0
Inter <hi@iinter.me>
2025-01-13 22:11:46 +0000

    🐛 fix: fix lack of spacing between dropdown items

--- src/components/ui/Select.tsx
+++ src/components/ui/Select.tsx
@@ -120,7 +120,7 @@ const SelectItem = React.forwardRef<
   <SelectPrimitive.Item
     ref={ref}
     className={cn(
-      "relative flex w-full cursor-default select-none items-center rounded-sm py-1.5 pl-2 pr-8 text-sm outline-none focus:bg-neutral-800 data-[disabled]:pointer-events-none data-[disabled]:opacity-50",
+      "relative flex w-full cursor-default select-none items-center rounded-sm py-1.5 pl-2 pr-8 mt-0.5 first:mt-0 text-sm outline-none focus:bg-neutral-800 data-[disabled]:pointer-events-none data-[disabled]:opacity-50",
       className
     )}
     {...props}
