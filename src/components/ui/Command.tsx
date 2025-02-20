6ddf75aefda62844bedcfc14d09f1f89dcfd40c4
Inter <hi@iinter.me>
2025-02-20 16:58:56 +0000

    🐛 fix: fix command menu showing above certain elements

--- src/components/ui/Command.tsx
+++ src/components/ui/Command.tsx
@@ -25,7 +25,7 @@ Command.displayName = CommandPrimitive.displayName
 const CommandDialog = ({ children, ...props }: DialogProps) => {
   return (
     <Dialog {...props}>
-      <DialogContent className="overflow-hidden border border-neutral-800 shadow-2xl shadow-neutral-950 p-0 w-11/12 max-w-xl">
+      <DialogContent className="overflow-hidden border border-neutral-800 shadow-2xl shadow-neutral-950 p-0 w-11/12 max-w-xl z-50">
         <Command className="[&_[cmdk-group-heading]]:text-stone-400 [&_[cmdk-group]:not([hidden])_~[cmdk-group]]:pt-0 [&_[cmdk-input-wrapper]_svg]:w-5 [&_[cmdk-input]]:h-12 [&_[cmdk-item]_svg]:h-5 [&_[cmdk-item]_svg]:w-5">
           {children}
         </Command>
